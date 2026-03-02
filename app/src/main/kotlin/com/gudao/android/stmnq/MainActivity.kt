34befa3489f68ecaa338255838a2caf9a4df7d6c
daren866 <105366647+daren866@users.noreply.github.com>
2026-03-02 21:32:16 +0800

    Add files via upload

---
+++ app/src/main/kotlin/com/gudao/android/stmnq/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.gudao.android.stmnq
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.gudao.android.stmnq.ui.AppTheme
+
+class MainActivity: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent { AppTheme { MainActivityContent() } }
+    }
+}
