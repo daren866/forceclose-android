14eca4e9e28ff3d3166c0f3588fbaa4bdda94f43
daren866 <105366647+daren866@users.noreply.github.com>
2026-03-03 18:43:30 +0800

    修复显示问题

--- app/src/main/kotlin/com/gudao/android/stmnq/MainActivity.kt
+++ app/src/main/kotlin/com/gudao/android/stmnq/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.gudao.android.stmnq.ui.AppTheme
+import com.gudao.android.stmnq.MainActivityContent
 
 class MainActivity: ComponentActivity() {
 
